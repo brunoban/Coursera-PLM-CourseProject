a9248f0b621ae9d605ea4186b0dc81ccba907cbd
brunoban <bruno.bandeira@gmail.com>
2015-02-22 20:52:53 -0300

    Final Commit
    Final Commit

--- misc2.R
+++
@@ -1,9 +0,0 @@
-library(caret)
-
-modFit <- train(classe ~ ., method="rpart",data=trainDB)
-print(modFit$finalModel)
-
-library(rattle)
-fancyRpartPlot(modFit$finalModel)
-
-predict(modFit,newdata=testing)   #party rpart tree bagEarth treeBag bagFda
